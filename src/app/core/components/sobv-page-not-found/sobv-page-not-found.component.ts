8737da06eb4a74ce9a6d5ad25622f9b0bbd29c33
alex <lolmagexe@gmail.com>
2022-12-22 12:22:04 +0200

    (feat) added not found page

--- src/app/core/components/sobv-page-not-found/sobv-page-not-found.component.ts
+++ src/app/core/components/sobv-page-not-found/sobv-page-not-found.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobv-page-not-found.component.scss']
 })
 export class SobvPageNotFoundComponent implements OnInit {
+  error_number: string = "";
+  error_name: string = "";
+  error_description: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.error_number = "404";
+    this.error_name = "Page Not Found";
+    this.error_description = "We`re sorry , the page you requested couldn`t be found Please go back home"
   }
 
 }
