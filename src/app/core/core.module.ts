d5182545331464c16363b9a3a1e025652358652b
Alex <lolmagexe@gmail.com>
2023-01-25 09:47:44 +0300

    (feat) added create platton modal form, updated variables, updated variables in header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,21 +1,26 @@
 import {NgModule} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {CommonModule} from "@angular/common";
 import {SobvCoreRoutingModule} from "./core-routing.module";
 import {SobvHeaderComponent} from "./components/sobv-header/sobv-header.component";
 import {SobvPageNotFoundComponent} from "./components/sobv-page-not-found/sobv-page-not-found.component";
 import {SobvDashboardComponent} from "./components/sobv-dashboard/sobv-dashboard.component";
 import {NgbTooltipModule} from "@ng-bootstrap/ng-bootstrap";
+import { SobvCreatePlatoonComponent } from './components/sobv-create-platoon/sobv-create-platoon.component';
 
 @NgModule({
   declarations: [
     SobvHeaderComponent,
     SobvPageNotFoundComponent,
-    SobvDashboardComponent
+    SobvDashboardComponent,
+    SobvCreatePlatoonComponent,
   ],
   imports: [
     CommonModule,
     SobvCoreRoutingModule,
-    NgbTooltipModule
+    NgbTooltipModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     SobvHeaderComponent,
