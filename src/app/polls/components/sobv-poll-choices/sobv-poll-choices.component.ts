6f7e33e4dc356367c542732f2dc253949d026f24
Andrii Zadvornyi <azadvornyi@deloitte.com>
2023-02-09 12:03:27 +0200

    (fix) fix bug with choices

--- src/app/polls/components/sobv-poll-choices/sobv-poll-choices.component.ts
+++ src/app/polls/components/sobv-poll-choices/sobv-poll-choices.component.ts
@@ -1,8 +1,5 @@
-import { Component } from '@angular/core';
-import {Observable} from "rxjs";
+import {Component, Input} from '@angular/core';
 import {Choice} from "../../interfaces";
-import {SobvPollsService} from "../../services/sobv-polls.service";
-import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'sobv-poll-choices',
@@ -10,23 +7,9 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./sobv-poll-choices.component.scss']
 })
 export class SobvPollChoicesComponent {
-  public choices$: Observable<Choice[]> | undefined;
+  @Input() choices: Choice[] | undefined;
 
-  constructor(
-    private sobvPollsService: SobvPollsService,
-    private route: ActivatedRoute) {
+  constructor() {
   }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-        if (!params.pollId) return
-        this.choices$ = this.getChoicesByPollId(params.pollId);
-      }
-    );
-
-  }
-
-  public getChoicesByPollId(pollId: number): Observable<Choice[]> {
-    return this.sobvPollsService.getChoicesByPollId(pollId);
-  }
 }
