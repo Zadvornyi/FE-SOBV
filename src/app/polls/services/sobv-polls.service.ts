880702671a427bec77f511af633dc0bbf6fea0fa
Andrii Zadvornyi <azadvornyi@deloitte.com>
2023-02-10 23:40:24 +0200

    (feat) add serviceman dropdown to subheader

--- src/app/polls/services/sobv-polls.service.ts
+++ src/app/polls/services/sobv-polls.service.ts
@@ -1,26 +1,29 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
-import {Poll, Choice, Question, Category} from "../interfaces";
+import {GlobalConstants} from '../../core/global-constants';
+import {Choice, Question, Category} from "../interfaces";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SobvPollsService {
-  // TODO: MAKE GLOBAL ENV VARIABLE
-  API_URL = 'http://127.0.0.1:8080/api';
   constructor(private http: HttpClient) {
   }
 
+  public getPollsCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${GlobalConstants.API_URL}/poll/categories`);
+  }
+
   public getPollsCategoryById(categoryId: number): Observable<Category> {
-    return this.http.get<Category>(`${this.API_URL}/poll/category/${categoryId}`);
+    return this.http.get<Category>(`${GlobalConstants.API_URL}/poll/category/${categoryId}`);
   }
 
   public getQuestionsByPollId(pollId: number): Observable<Question[]> {
-    return this.http.get<Question[]>(`${this.API_URL}/poll/${pollId}/question`);
+    return this.http.get<Question[]>(`${GlobalConstants.API_URL}/poll/${pollId}/question`);
   }
-  
+
   public getChoicesByPollId(pollId: number): Observable<Choice[]> {
-    return this.http.get<Choice[]>(`${this.API_URL}/poll/${pollId}/choice`);
+    return this.http.get<Choice[]>(`${GlobalConstants.API_URL}/poll/${pollId}/choice`);
   }
 }
--- src/app/profile-serviceman/profile-serviceman.module.ts
+++ src/app/profile-serviceman/profile-serviceman.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SobvProfileServicemanRoutingModule} from "./profile-serviceman-routing.module";
 import {SobvProfileServicemanComponent} from "./components/sobv-profile-serviceman/sobv-profile-serviceman.component";
+import {CoreModule} from "../core/core.module";
+import {NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -9,7 +11,9 @@ import {SobvProfileServicemanComponent} from "./components/sobv-profile-servicem
   ],
   imports: [
     CommonModule,
-    SobvProfileServicemanRoutingModule
+    SobvProfileServicemanRoutingModule,
+    CoreModule,
+    NgbDropdownModule
   ],
   exports: [
     SobvProfileServicemanComponent
