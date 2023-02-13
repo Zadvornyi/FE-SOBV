bf5331215b37c9bf1f51d3dd387b538623060414
Oleksis <68026546+OleksisBondarenko@users.noreply.github.com>
2023-02-14 01:09:41 +0200

    Ocherevan task modal popup (#20)
    * (feat) added simple full screen popup
    
    * (feat) added header control
    
    * (feat) updated styles for poll-modal, fix name core variables
    
    * (feat) styled modal component, added mock description

--- src/app/sobv-routing.module.ts
+++ src/app/sobv-routing.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SobvDashboardComponent} from "./core/components/sobv-dashboard/sobv-dashboard.component";
 import {SobvPageNotFoundComponent} from "./core/components/sobv-page-not-found/sobv-page-not-found.component";
+import { SobvPollModalPopupComponent } from './polls/components/sobv-poll-modal-popup/sobv-poll-modal-popup.component';
 import {SobvPollModalComponent} from "./polls/components/sobv-poll-modal/sobv-poll-modal.component";
 import {SobvProfileServicemanComponent} from "./profile-serviceman/components/sobv-profile-serviceman/sobv-profile-serviceman.component";
 
 const routes: Routes = [
   {path: 'dashboard', component: SobvDashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'polls/category/:categoryId/poll/:pollId', component: SobvPollModalComponent},
+  {path: 'polls/category/:categoryId/poll/:pollId', component: SobvPollModalPopupComponent},
   {path: 'profile/serviceman/:id', component: SobvProfileServicemanComponent},
   {path: 'error/:code', component: SobvPageNotFoundComponent},
   {path: '**', component: SobvPageNotFoundComponent}
