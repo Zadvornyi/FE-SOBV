d662f1d762efdecaaff7561c19fb7fecc18906d9
Alex <lolmagexe@gmail.com>
2023-05-08 11:09:59 +0300

    (fix) solved problem with compability of branches

--- src/app/profile-serviceman/profile-serviceman.module.ts
+++ src/app/profile-serviceman/profile-serviceman.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SobvProfileServicemanComponent} from "./components/sobv-profile-serviceman/sobv-profile-serviceman.component";
 import {CoreModule} from "../core/core.module";
 import {NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
-import {
-  SobvCreateServicemanComponent
-} from "../core/components/sobv-create-serviceman/sobv-create-serviceman.component";
+import {SobvProfileServicemanComponent} from "./components/sobv-profile-serviceman/sobv-profile-serviceman.component";
 
 @NgModule({
   declarations: [
     SobvProfileServicemanComponent,
-    SobvCreateServicemanComponent,
   ],
   imports: [
     CommonModule,
     NgbDropdownModule,
-    CoreModule
+    CoreModule,
   ],
   exports: [
     SobvProfileServicemanComponent,
