2990908e692310082d117d3ae0a24c4a0ea7c926
Jokail <jokailxx@gmail.com>
2025-05-08 08:56:00 +0000

    add: sobv company/platoon service

---
+++ src/app/core/services/sobv-platoon.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GlobalConstants } from '../global-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SobvPlatoonService {
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Create a new platoon
+   * @param data - The platoon data
+   * @returns Observable of the created platoon
+   */
+  createPlatoon(data: { sequence_number: number, description?: string, commander?: string }): Observable<any> {
+    return this.http.post(`${GlobalConstants.API_URL}/api/platoon`, data);
+  }
+
+  /**
+   * Get all platoons
+   * @return Observable of all platoons
+   */
+  getPlatoons(): Observable<any> {
+    return this.http.get(`${GlobalConstants.API_URL}/api/platoon`);
+  }
+
+  /**
+   * Get a platoon by ID
+   * @param id - The platoon ID
+   * @return Observable of the platoon
+   */
+  getPlatoonById(id: string): Observable<any> {
+    return this.http.get(`${GlobalConstants.API_URL}/api/platoon/${id}`);
+  }
+
+  /**
+   * Update a platoon
+   * @param id - The platoon ID
+   * @param data - The updated platoon data
+   * @return Observable of the updated platoon
+   */
+  updatePlatoon(id: string, data: any): Observable<any> {
+    return this.http.put(`${GlobalConstants.API_URL}/api/platoon/${id}`, data);
+  }
+
+  /**
+   * Delete a platoon
+   * @param id - The platoon ID
+   * @return Observable of the deletion result
+   */
+  deletePlatoon(id: string): Observable<any> {
+    return this.http.delete(`${GlobalConstants.API_URL}/api/platoon/${id}`);
+  }
+}
